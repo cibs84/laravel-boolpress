0df287e1d26e0b1fb288eafeb39d183afd2fb5b1
cibs84 <d.tilesi@gmail.com>
2022-09-05 15:57:02 +0200

    created posts table with fake data

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
+use Illuminate\Support\Str;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 15; $i++) { 
+            $new_post = new Post();
+            $new_post->title = Str::of($faker->words(rand(3, 5), true))->ucfirst();
+            $new_post->slug = Str::of($new_post->title)->lower()->slug('-');
+            $new_post->content = $faker->paragraphs(rand(7, 15), true);
+            $new_post->save();
+        }
+    }
+}
